f2a3cd00a6f7933309a6850110ec13975598f1ce
Daniel McPherson <daniel@daniels-mbp.home>
2022-05-26 20:22:13 +0100

    Added the title, making the price display

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,32 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+                child: const Text(
+                  "Tip Calculator",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 204, 51, 255)
+                  ),
+                ),
+              ),
+              const Text(
+                "£57.00",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              )
+            ]
+          ),
+        ),
+      )
     );
   }
 }
