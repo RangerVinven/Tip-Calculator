6c7acc97d539a53a510206a8221373a1681b5090
Daniel McPherson <daniel@daniels-mbp.home>
2022-05-26 21:26:08 +0100

    Added a sized box under the button

--- lib/main.dart
+++ lib/main.dart
@@ -72,7 +72,8 @@ class _HomePageState extends State<HomePage> {
                       )
                     ],
                   )
-                )
+                ),
+                const SizedBox(height: 10)
               ],
             ),
           ),
